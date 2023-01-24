7d0e0eea8d3cedb60636ebbbbe0441c9fd7a81a2
Atiqul Hoque <atiqbaqi@gmail.com>
2023-01-24 12:29:51 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const app_port=process.env.APP_PORT || 8081;
+// Tell Express to use EJS as the template engine
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(app_port,()=>{
+    console.log(`listening on ${app_port}`);
+});
