5e4b7844d50b5330e00ceb261e7912988f29e373
Atiqul Hoque <atiqbaqi@gmail.com>
2023-01-25 15:57:30 +0600

    datatables pagination working

--- controller/EventController.js
+++ controller/EventController.js
@@ -40,7 +40,52 @@ module.exports = {
 
     async listEvents(req,res){
         try {
-            let token;
+            // let token;
+            // const page = req.query.start;
+            // const size = req.query.length;
+            // console.log(page+'>>'+size);
+            // await axios_instance.post('/api/login',{user_name:process.env.API_USER,password:process.env.API_PASSWORD})
+            // .then((response) => {
+            //     token = response.data.token;
+            // }).catch((error) => {
+            //     console.error(error);
+            // });
+
+            // const list_event = {
+            //     method: 'get',
+            //     url: '/api/events',
+            //     headers: { 
+            //         'Authorization': 'Bearer ' + token, 
+            //         'Content-Type': 'application/json'
+            //     },
+            //     params:{
+            //         page:page||1,
+            //         size:size||5
+            //     }
+            // };
+
+            // let data;
+            // await axios_instance(list_event)
+            // .then((response)=>{
+            //     data = response.data;
+            // }).catch((err)=>{
+            //     console.log(err);
+            // });
+            // return res.render('events',data);
+            return res.render('events');
+        } catch (error) {
+            console.log(error);
+            return res.render('events');
+        }
+    },
+
+    async serverSideProcessing(req,res){
+        try {
+            const start = parseInt(req.query.start);
+            const size = parseInt(req.query.length);
+            let page = Math.floor(start / size) + 1;
+            const draw = parseInt(req.query.draw);
+            console.log(page+'>>'+size+'>>'+draw);
             await axios_instance.post('/api/login',{user_name:process.env.API_USER,password:process.env.API_PASSWORD})
             .then((response) => {
                 token = response.data.token;
@@ -56,19 +101,26 @@ module.exports = {
                     'Content-Type': 'application/json'
                 },
                 params:{
-                    page:req.query.page||1,
-                    size:req.query.size||5
+                    page:page||1,
+                    size:size||5
                 }
             };
 
-            let data;
+            let result;
             await axios_instance(list_event)
             .then((response)=>{
-                data = response.data;
+                result = response.data;
             }).catch((err)=>{
                 console.log(err);
             });
-            return res.render('events',data);
+            
+            let dataArray = (result.data).map(function(obj) {
+                return Object.values(obj);
+            });
+            result.data = dataArray;
+            // result.recordsTotal = 14;
+            result.recordsFiltered = result.recordsTotal;
+            res.json(result);
         } catch (error) {
             console.log(error);
             return res.render('events');
--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,12 @@ router.get('/create', async (req, res)=> {
     res.render('create');
 });
 
+//create a new event
 router.post('/create', EventController.createEvent);
+
+//list events
 router.get('/events', EventController.listEvents);
+router.get('/server-side-processing',EventController.serverSideProcessing);
+  
 
 module.exports=router;
